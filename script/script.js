6d1929e13e62a851f0fec3cf6815fbdb33b53d70
Spynu Jean <jspynu14@mail.ru>
2021-11-17 09:33:34 +0400

    bugfix3

--- script/script.js
+++ script/script.js
@@ -1,29 +1,24 @@
-/*form_open*/
-let formContainer = document.querySelector('.form-container');
-let pop_up = document.querySelector('.pop-up');
+let popUp = document.querySelector('.pop-up');
 let formElement = document.querySelector('.form');
 let edit = document.querySelector('.profile__edit-button');
-let close = document.querySelector('.exit');
+let close = document.querySelector('.pop-up__exit');
 let profileName = document.querySelector('.profile__name');
 let profileSubName = document.querySelector('.profile__subname');
 let nameInput = document.querySelector('.form__input_profile_name');
 let jobInput = document.querySelector('.form__input_profile_subname');
 
-
 //открытие попапа
-function clicked() {
+function open() {
 
     profileName.textContent = nameInput.value;
     profileSubName.textContent = jobInput.value;
-    formContainer.classList.add('form-container_display_block');
-    pop_up.classList.add('pop-up_display_flex');
+    popUp.classList.add('pop-up_opened');
 
 }
 //закрытие попапа
 function exit() {
 
-    formContainer.classList.remove('form-container_display_block');
-    pop_up.classList.remove('pop-up_display_flex');
+    popUp.classList.remove('pop-up_opened');
 
 }
 //сохранение формы
@@ -32,10 +27,9 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileSubName.textContent = jobInput.value;
-
+    exit();
 }
 
-edit.addEventListener('click', clicked);
+edit.addEventListener('click', open);
 close.addEventListener('click', exit);
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', exit);
